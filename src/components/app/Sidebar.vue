d072a25a3d709393b6fe7f67b83ed83822a4cf41
Tarashevich <riot121212@gmail.com>
2020-12-03 20:43:12 +0200

    first commit

---
+++ src/components/app/Sidebar.vue
@@ -0,0 +1,29 @@
+<template>
+    <ul class="sidenav app-sidenav open">
+
+        <router-link
+                v-for="link in links"
+                :key="link.url"
+                tag="li"
+                :to="link.url"
+                :exact="link.exact"
+                active-class="active"
+        >
+            <a class="waves-effect waves-orange pointer">{{link.title}}</a>
+        </router-link>
+    </ul>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            links: [
+                {url:'/', title:'Счет', exact: true},
+                {url:'/history', title:'История'},
+                {url:'/planning', title:'Планирование'},
+                {url:'/record', title:'Новая запись'},
+                {url:'/categories', title:'Категории'}
+            ]
+        })
+    }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -8,15 +8,56 @@ const routes = [
   {
     path: '/',
     name: 'Home',
+    meta: {layout: 'main'},
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'login',
+    meta: {layout: 'empty'},
+    component: () => import('../views/Login')
+  },
+  {
+    path: '/register',
+    name: 'register',
+    meta: {layout: 'empty'},
+    component: () => import('../views/Register')
+  },
+  {
+    path: '/detail-record',
+    name: 'detail-record',
+    meta: {layout: 'main'},
+    component: () => import('../views/DetailRecord')
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: {layout: 'main'},
+    component: () => import('../views/Record')
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: {layout: 'main'},
+    component: () => import('../views/History')
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: {layout: 'main'},
+    component: () => import('../views/Planning')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: {layout: 'main'},
+    component: () => import('../views/Profile')
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    meta: {layout: 'main'},
+    component: () => import('../views/Categories')
   }
 ]
 
